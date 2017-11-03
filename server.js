4998b303f4ec60939903350ae3b3eb76c24e0ef7
Sanjay Murali <sanjaymurali@ccs.neu.edu>
2017-11-03 00:54:06 -0400

    Added Server

---
+++ server.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+// Redirect all other routes to index.html and let angular deal with it
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+//#endregion: routes
+
+const port = process.env.PORT || '3100';
+app.set('port', port);
+
+app.listen(port);
